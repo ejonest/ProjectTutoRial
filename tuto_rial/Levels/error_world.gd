d36877c007af48c953aa7d9a8ed7e89d699233cd
ejonest <70674232+ejonest@users.noreply.github.com>
2024-04-25 22:28:55 -0700

    Special ability added to error scene
    f key will now light the path to the exit for error scene but they go off after 3 seconds and ability has a 10 second cooldown

--- tuto_rial/Levels/error_world.gd
+++ tuto_rial/Levels/error_world.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var slimeWhite = load("res://Enemies/slimeWhite.tscn")
 var enemyArray : Array
 var exitPortal
+var canFlipLights = true
 
 func _ready():
 	spawnenemy(Vector3(4, 0, -110), Vector3(5, 5, 5), 30, 2, false, slimeWhite)
@@ -17,6 +18,8 @@ func _ready():
 	spawnenemy(Vector3(8, 0, -30), Vector3(5, 5, 5), 30, 2, false, slimeWhite)
 	exitPortal = get_tree().get_nodes_in_group("Portal")[1]
 	exitPortal.changeScene.connect(ReturnHome)
+	for n in get_tree().get_nodes_in_group("Lights"):
+				n.visible = false
 	pass
 
 func _process(delta):
@@ -26,6 +29,8 @@ func _process(delta):
 			n.queue_free()
 			enemyArray.pop_at(i)
 		i += 1
+	if Input.is_action_just_pressed("path") && canFlipLights:
+		flipLights()
 	pass
 
 func ReturnHome():
@@ -34,6 +39,18 @@ func ReturnHome():
 	get_tree().change_scene_to_file.bind("res://main.tscn").call_deferred()
 	print("should have exited")
 	pass
+	
+func flipLights():
+	canFlipLights = false
+	for n in get_tree().get_nodes_in_group("Lights"):
+			n.visible = true
+	await get_tree().create_timer(3).timeout
+	for n in get_tree().get_nodes_in_group("Lights"):
+			n.visible = false
+	print("waiting")
+	await get_tree().create_timer(10).timeout
+	print("Done waiting")
+	canFlipLights = true
 	
 func spawnenemy(pos : Vector3, scale : Vector3, health : int, speed : float, canTakeDamage : bool, Type):
 	var enemy = Type.instantiate()
@@ -44,11 +61,9 @@ func spawnenemy(pos : Vector3, scale : Vector3, health : int, speed : float, can
 	enemy.takeDamage(canTakeDamage)
 	add_child(enemy)
 	var children = get_children()
-	print("Size A: ", enemyArray.size())
-	for n in children:
-		if n.is_in_group("Enemy"):
-			enemyArray.append(n)
-	print("Size B: ", enemyArray.size())
+	#print("Size A: ", enemyArray.size())
+	enemyArray.append(enemy)
+	#print("Size B: ", enemyArray.size())
 	pass
 
 func removeenemy():
--- tuto_rial/main.gd
+++ tuto_rial/main.gd
@@ -12,7 +12,7 @@ var mazePortal
 func _ready():
 # To spawn an enemy you need to pass it a location(vector3), a scaling(vector3), 
 # health(int), speed(float), if they can take damage(bool), and the type of slime
-	spawnenemy(Vector3(-5, 1, -12), Vector3(5, 5, 5), 30, 1.5, true, slimePlain)
+	spawnenemy(Vector3(-5, 1, -12), Vector3(5, 5, 5), 10, 1.5, true, slimePlain)
 	spawnenemy(Vector3(5, 1, -12), Vector3(5, 5, 5), 10, 1.5, true, slimeTeal)
 	spawnenemy(Vector3(-3, 5, -40), Vector3(5, 5, 5), 30, 1.5, true, slimeWhite)
 	spawnenemy(Vector3(3, 5, -40), Vector3(5, 5, 5), 10, 1.5, true, slimeRock)
@@ -26,11 +26,12 @@ func _ready():
 
 func _process(delta):
 	var i = 0
-	for n in enemyArray:
-		if n.getHealth() <= 0:
-			n.queue_free()
-			enemyArray.pop_at(i)
-		i += 1
+	if enemyArray.size() > 0:
+		for n in enemyArray:
+			if n.getHealth() <= 0:
+				n.queue_free()
+				enemyArray.pop_at(i)
+			i += 1
 	pass
 
 func ForestScene():
@@ -63,11 +64,12 @@ func spawnenemy(pos : Vector3, scale : Vector3, health : int, speed : float, can
 	enemy.takeDamage(canTakeDamage)
 	add_child(enemy)
 	var children = get_children()
-	print("Size A: ", enemyArray.size())
-	for n in children:
-		if n.is_in_group("Enemy"):
-			enemyArray.append(n)
-	print("Size B: ", enemyArray.size())
+	enemyArray.append(enemy)
+	#print("Size A: ", enemyArray.size())
+	#for n in children:
+		#if n.is_in_group("Enemy"):
+			#enemyArray.append(n)
+	#print("Size B: ", enemyArray.size())
 	pass
 
 func removeenemy():
